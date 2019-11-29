e6902d2d05095e8cee94ac335997704b66d2bdab
Dennis Mercado <dennismercado@Denniss-Mac-Pro.local>
2019-11-29 05:41:36 -0600

    trying to deploy postgresql to heroku

--- knexfile.js
+++ knexfile.js
@@ -22,9 +22,7 @@ module.exports = {
 
   staging: {
     client: "pg",
-    connection: {
-      database: "process.env.DATABASE_URL"
-    },
+    connection: "process.env.DATABASE_URL",
     pool: {
       min: 2,
       max: 10
@@ -39,9 +37,8 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: {
-      database: "process.env.DATABASE_URL"
-    },
+    connection: "process.env.DATABASE_URL",
+
     pool: {
       min: 2,
       max: 10
